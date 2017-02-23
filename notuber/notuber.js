f3be5d798daeb98c2d2fecd68be9666290fdf749
Michelle Luo <luo.mich16@gmail.com>
2017-02-22 19:05:04 -0500

    display own location on map & POST location

---
+++ notuber/notuber.js
@@ -0,0 +1,47 @@
+/* comp20 a2 - js for notuber
+ * Michelle Luo (mluo02)
+ */
+
+function getLocation() {
+    var req = new XMLHttpRequest();
+    var dest = "https://defense-in-derpth.herokuapp.com/submit";
+    var data;
+    var map;
+    var where;
+    var marker;
+    var lat;
+    var lng;
+
+    /* getting location from HTML5 API */
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+            data = "username=TRAM96zq&lat=" + lat + "&lng=" + lng;
+
+            /* using location data to make Google Maps map */
+            where = new google.maps.LatLng(lat, lng);
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: where,
+                zoom: 17
+            });
+            /* marker -- testing purposes */
+            marker = new google.maps.Marker({
+                position: where,
+                map: map
+            })
+            /* send data to datastore -- which can only be done after
+             * location data is retrieved */
+            req.open("POST", dest, true);
+            req.setRequestHeader("Content-type",
+                "application/x-www-form-urlencoded");
+            req.send(data);
+        });
+    }
+
+    req.onload = function () {
+        /* parse json */
+        console.log(this.responseText);
+    }
+
+}
