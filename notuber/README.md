5e3c18e15e787859cc668e3c0916d36454d63942
Michelle Luo <luo.mich16@gmail.com>
2017-03-06 16:02:47 -0500

    Display info in a single info window

--- notuber/README.md
+++ notuber/README.md
@@ -1,1 +1,12 @@
+## comp20 a2: notuber
+# Michelle Luo (mluo02)
+# 3.6.17
+
+In this assignment I send a request to the server and receive a JSON containing
+vehicle data. Then using the Google Maps API, I display my own location and all
+the vehicles' location on a map. To see the distance between the passenger and
+a car, click the car.
+
 Thanks to iconfinder.com for the passenger icon.
+
+I spent approximately 4 hours on this assignment.
--- notuber/notuber.js
+++ notuber/notuber.js
@@ -3,35 +3,7 @@
  * Michelle Luo (mluo02)
  */
 
-function addVehicleMarkers(passenger, map, vehicles, img) {
-    for (var i = 0, v; v = vehicles[i]; i++) {
-        var pos = new google.maps.LatLng(v.lat, v.lng);
-        var dist = google.maps.geometry.spherical.computeDistanceBetween(
-                    passenger, pos);
-        var contentString = '<div id="content"><h4 id="firstHeading">' +
-            v._id + "</h4>" + "<h5>distance from you:</h5>" + dist +
-            "<p>" + pos.lat() + ", " +
-            pos.lng() + "</p></div>";
-
-        addMarker(map, pos, img, contentString);
-    }
-}
-
-function addMarker(map, position, img, contentString) {
-    var marker = new google.maps.Marker({
-        position: position,
-        icon: img,
-        map: map
-    });
-
-    var info = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    marker.addListener('click', function() {
-        info.open(map, this);
-    });
-}
+var METERS_TO_MILES = 0.000621371;
 
 function getLocation() {
     var req = new XMLHttpRequest();
@@ -49,17 +21,21 @@ function getLocation() {
             lat = position.coords.latitude;
             lng = position.coords.longitude;
 
-            /* using location data to make Google Maps map */
+            /* use location data to make Google Maps map */
             passenger = new google.maps.LatLng(lat, lng);
             map = new google.maps.Map(document.getElementById("map"), {
                 center: passenger,
                 zoom: 17
             });
-            var contentString = '<div id="content"><h4 id="firstHeading">' +
-                user.username + "</h4>" + "<p>" + lat + ", " + lng +
+            var contentString = '<div id="content"><h3 id="firstHeading">' +
+                user.username + "</h3>" + "<p>" + lat + ", " + lng +
                 "</p></div>";
 
-            addMarker(map, passenger, "passenger.png", contentString);
+            var info = new google.maps.InfoWindow({
+                    content: contentString
+            });
+            /* put passenger on map */
+            addMarker(passenger, "passenger.png", map, contentString, info);
 
             /* send data to datastore -- which can only be done after
              * location data is retrieved */
@@ -69,7 +45,8 @@ function getLocation() {
             req.onload = function () {
                 var res = JSON.parse(this.responseText);
                 vehicles = res.vehicles;
-                addVehicleMarkers(passenger, map, vehicles, "black_car.png");
+                addVehicleMarkers(passenger, map, vehicles,
+                    "black_car.png", info);
             }
             data = "username=" + user.username + "=" + lat + "&lng=" + lng;
             req.send(data);
@@ -78,4 +55,36 @@ function getLocation() {
     else {
         alert("Your browser doesn't support geolocation :(");
     }
+}
+
+
+function addVehicleMarkers(passenger, map, vehicles, img, info)
+{
+    var pos;
+    var dist;
+    var contentString;
+    for (var i = 0, v; v = vehicles[i]; i++) {
+        pos = new google.maps.LatLng(v.lat, v.lng);
+        dist = METERS_TO_MILES *
+                    google.maps.geometry.spherical.computeDistanceBetween(pos,
+                    passenger);
+        dist = dist.toFixed(4);
+        contentString = '<div id="content"><h3 id="firstHeading">' +
+            v._id + "</h3>" + "<p>distance from you: " + dist + " mi</p>";
+        addMarker(pos, img, map, contentString, info);
+    }
+}
+
+function addMarker(pos, img, map, contentString, info) {
+    var marker = new google.maps.Marker({
+        position: pos,
+        icon: img,
+        map: map
+    });
+    /* attach listener to marker - changes content of the infowindow,
+     * then sets infowindow at the clicked marker */
+    marker.addListener('click', function() {
+        info.setContent(contentString);
+        info.open(map, this);
+    });
 }
