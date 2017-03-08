5d7441e7c43f9574765829a1edae222e0d3e4408
Michelle Luo <luo.mich16@gmail.com>
2017-03-08 13:44:39 -0500

    Handle case of switching roles

--- notuber/notuber.js
+++ notuber/notuber.js
@@ -10,43 +10,54 @@ function getLocation() {
     var dest = "https://defense-in-derpth.herokuapp.com/submit";
     var data;
     var map;
-    var passenger;
+    var me;
+    var myImg;
     var lat;
     var lng;
-    var vehicles;
+    var arr;
+    var them;
+    var theirImg;
 
     /* getting location from HTML5 API */
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             lat = position.coords.latitude;
             lng = position.coords.longitude;
-
-            /* use location data to make Google Maps map */
-            passenger = new google.maps.LatLng(lat, lng);
-            map = new google.maps.Map(document.getElementById("map"), {
-                center: passenger,
-                zoom: 17
-            });
-            var contentString = '<div id="content"><h3 id="firstHeading">' +
-                user.username + "</h3>" + "<p>" + lat + ", " + lng +
-                "</p></div>";
-
-            var info = new google.maps.InfoWindow({
-                    content: contentString
-            });
-            /* put passenger on map */
-            addMarker(passenger, "passenger.png", map, contentString, info);
-
             /* send data to datastore -- which can only be done after
              * location data is retrieved */
             req.open("POST", dest, true);
             req.setRequestHeader("Content-type",
                 "application/x-www-form-urlencoded");
+
             req.onload = function () {
                 var res = JSON.parse(this.responseText);
-                vehicles = res.vehicles;
-                addVehicleMarkers(passenger, map, vehicles,
-                    "black_car.png", info);
+                if (res.hasOwnProperty('vehicles')) {
+                    them = res.vehicles;
+                    myImg = "passenger.png";
+                    theirImg = "black_car.png";
+                }
+                else {
+                    them = res.passengers;
+                    myImg = "black_car.png";
+                    theirImg = "passenger.png";
+                }
+
+                /* use location data to make Google Maps map */
+                me = new google.maps.LatLng(lat, lng);
+                map = new google.maps.Map(document.getElementById("map"), {
+                    center: me,
+                    zoom: 17
+                });
+                var contentString = '<div id="content"><h3 id="firstHeading">' +
+                    user.username + "</h3>" + "<p>" + lat + ", " + lng +
+                    "</p></div>";
+                var info = new google.maps.InfoWindow({
+                        content: contentString
+                });
+
+                /* put myself on map */
+                addMarker(me, myImg, map, contentString, info);
+                addVehicleMarkers(me, map, them, theirImg, info);
             }
             data = "username=" + user.username + "=" + lat + "&lng=" + lng;
             req.send(data);
@@ -58,16 +69,16 @@ function getLocation() {
 }
 
 
-function addVehicleMarkers(passenger, map, vehicles, img, info)
+function addVehicleMarkers(me, map, them, img, info)
 {
     var pos;
     var dist;
     var contentString;
-    for (var i = 0, v; v = vehicles[i]; i++) {
+    for (var i = 0, v; v = them[i]; i++) {
         pos = new google.maps.LatLng(v.lat, v.lng);
         dist = METERS_TO_MILES *
                     google.maps.geometry.spherical.computeDistanceBetween(pos,
-                    passenger);
+                    me);
         dist = dist.toFixed(4);
         contentString = '<div id="content"><h3 id="firstHeading">' +
             v._id + "</h3>" + "<p>distance from you: " + dist + " mi</p>";
